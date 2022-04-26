526e785c1555b03d0f401fab9dd223ce377162d6
Astronomyyyy <90847686+Astronomyyyy@users.noreply.github.com>
2022-04-26 12:10:27 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+from PIL import Image
+
+def load_img(filepath):
+    img = Image.open(filepath).convert('RGB')
+    img.resize((256,256),Image.BICUBIC)
+    return img
+
+def is_img_file(filename):
+    for extension in ['.jpeg','.jpg','.png']:
+        return any(filename.endswith(extension))
+
+def save_img(img_tensor,filename):
+    img_numpy = img_tensor.float().numpy()
+    img_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
+    img_numpy = img_numpy.clip(0,255).astype(np.uint8)
+    img = Image.fromarray(img_numpy)
+    img.save(filename)
+    print("Image saved as {}".format(filename))
+
+
+
+
+
+
